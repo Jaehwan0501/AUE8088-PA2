f50c4029eeb3b8b3f2738c23d9c6bb709ba0730c
jaehwan <jaehwan@.com>
2024-06-13 18:32:32 +0900

    Add wandb account data in train_simple.py

--- train_simple.py
+++ train_simple.py
@@ -26,6 +26,8 @@ from pathlib import Path
 from torch.optim import lr_scheduler
 from tqdm import tqdm
 
+import wandb
+
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
@@ -481,5 +483,11 @@ def main(opt, callbacks=Callbacks()):
 
 
 if __name__ == "__main__":
+    wandb.login(key='61103c383e2466af37dce7fb530359a81de3f2b4', relogin=True)
+    wandb.init(
+        # set the wandb project where this run will be logged
+        project="deep_learning_class_pa2_hyu"
+    )
+
     opt = parse_opt()
     main(opt)
